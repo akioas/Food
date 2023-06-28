3cb22803a0b549a1e63f472fd535d2c841fcf267
Ekaterina Velgusheva <evelgusheva@rts-soft.ru>
2023-06-28 15:43:33 +0300

    Initial Commit

---
+++ Food/Coordinator/ContentView.swift
@@ -0,0 +1,20 @@
+import SwiftUI
+
+
+struct ContentView: View {
+    @StateObject private var coordinator = Coordinator
+    
+    var body: some View {
+        ZStack {
+            coordinator.container.view
+                .transition(.push(from: .bottom))
+        }
+        .preferredColorScheme(.light)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
